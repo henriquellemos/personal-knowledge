116067ea7cba1210672ce29e5c1829b3f4986eb8
Henrique <henriquellemos@gmail.com>
2026-03-14 10:26:00 -0300

    feat: initial commit, structure of project

---
+++ api/src/main/java/com/personal/knowledge/api/PersonalKnowledgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.knowledge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalKnowledgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalKnowledgeApiApplication.class, args);
+	}
+
+}
